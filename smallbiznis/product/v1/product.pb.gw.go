e79ea080947d4313617194693cb3b5fdf6a9d799
Small Biznis APIs <runner@fv-az1445-662.i1cbdw21p5ze5pcdzcvy21hj0h.bx.internal.cloudapp.net>
2024-06-07 20:46:11 +0000

    regenerate API

--- smallbiznis/product/v1/product.pb.gw.go
+++ smallbiznis/product/v1/product.pb.gw.go
@@ -67,10 +67,6 @@ func local_request_Service_ListCategory_0(ctx context.Context, marshaler runtime
 
 }
 
-var (
-	filter_Service_GetCategory_0 = &utilities.DoubleArray{Encoding: map[string]int{"category_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
-)
-
 func request_Service_GetCategory_0(ctx context.Context, marshaler runtime.Marshaler, client ServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetCategoryRequest
 	var metadata runtime.ServerMetadata
@@ -90,13 +86,6 @@ func request_Service_GetCategory_0(ctx context.Context, marshaler runtime.Marsha
 	protoReq.CategoryId, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "category_id", err)
-	}
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Service_GetCategory_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetCategory(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -123,13 +112,6 @@ func local_request_Service_GetCategory_0(ctx context.Context, marshaler runtime.
 	protoReq.CategoryId, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "category_id", err)
-	}
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Service_GetCategory_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetCategory(ctx, &protoReq)
