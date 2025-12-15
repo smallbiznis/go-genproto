4ca5da118fb9fc9349635fb9ac28071adc39f2f9
Taufik Triantono <taufiktriantono4@gmail.com>
2025-12-15 06:43:25 +0000

    regenerate API

--- smallbiznis/meter/v1/meter.pb.gw.go
+++ smallbiznis/meter/v1/meter.pb.gw.go
@@ -65,6 +65,10 @@ func local_request_MeterService_CreateMeter_0(ctx context.Context, marshaler run
 
 }
 
+var (
+	filter_MeterService_GetMeter_0 = &utilities.DoubleArray{Encoding: map[string]int{"id": 0}, Base: []int{1, 2, 0, 0}, Check: []int{0, 1, 2, 2}}
+)
+
 func request_MeterService_GetMeter_0(ctx context.Context, marshaler runtime.Marshaler, client MeterServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetMeterRequest
 	var metadata runtime.ServerMetadata
@@ -84,6 +88,13 @@ func request_MeterService_GetMeter_0(ctx context.Context, marshaler runtime.Mars
 	protoReq.Id, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MeterService_GetMeter_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetMeter(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -110,6 +121,13 @@ func local_request_MeterService_GetMeter_0(ctx context.Context, marshaler runtim
 	protoReq.Id, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MeterService_GetMeter_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetMeter(ctx, &protoReq)
